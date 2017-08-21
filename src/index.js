0b1b7f7175332700e6911da130d801b220e53272
Unknown <axelsheva94@gmail.com>
2017-08-21 15:17:21 +0300

    Add redux-devtools-extension

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 import './index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
